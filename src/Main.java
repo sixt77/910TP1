3651d5e47120abbc07212eac712b6f3f9a8f7873
sixt77 <Vontriknof78>
2020-11-10 11:40:57 +0100

    1commit

--- src/Main.java
+++ src/Main.java
@@ -6,9 +6,11 @@ public class Main {
     public static void main(String[] args) {
         Occurencies oc = FileTool.buildOccurencies("C:\\Users\\sacha\\test.save");
         String data = FileTool.readFile("C:\\Users\\sacha\\test1.txt");
-        //oc.print();
+        double sum = 0;
         for(int i = 0; i < data.length()-3; i++){
             System.out.println(oc.getProba(data.substring(i, i+4)));
+            sum = sum + oc.getProba(data.substring(i, i+4));
         }
+        System.out.println(sum);
     }
 }
